7c646e0bfa06acc2d3e0eaf5b4660741332beaa4
Jace <livingfern1@live.com>
2023-03-08 07:40:22 -0500

    Update to 3.5.1
    Update to 3.5.1

--- src/MedAttention.ts
+++ src/MedAttention.ts
@@ -1,8 +1,8 @@
 /**
  *      Name: Medical Attention
- *   Version: 350.0.3
+ *   Version: 351.0.1
  * Copyright: jbs4bmx
- *    Update: [DMY] 23.02.2023
+ *    Update: [DMY] 07.03.2023
 */
 
 import { DependencyContainer } from "tsyringe";
