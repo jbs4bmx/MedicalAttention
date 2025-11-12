ce4c89bb0a10656eba1e139ac26f5fe6311456b9
Jace <livingfern1@live.com>
2025-11-11 22:39:37 -0500

    fix version

--- project/MedicalAttention-Server/MedServer.cs
+++ project/MedicalAttention-Server/MedServer.cs
@@ -22,7 +22,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "MedicalAttention";
     public override string Author { get; init; } = "jbs4bmx";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("4.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("4.0.1");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
